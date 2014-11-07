65e2d11bce250a695fed762e1458738738324f10
Nicholas Herson <nicholas.herson@gmail.com>
2014-11-06 20:24:44 -0800

    remove old method, keep better method

--- code/shotgun-sequence.py
+++ code/shotgun-sequence.py
@@ -1,5 +1,6 @@
 import fileinput
-import Queue
+
+# NOTE: Developed on Python 2.7.6
 
 # All of the short reads
 # At each iteration, this list will have
@@ -7,12 +8,9 @@ import Queue
 # of the two short reads -- the two with the largest overlap
 reads = []
 
-# A priority queue keeping track of
-# the max overlap between pairs of strings
-queue = Queue.PriorityQueue()
-
 # Finds the shortest superstring
 # of the two given strings
+# runtime = O(k^2)
 def find_shortest_superstring(s1, s2):
   # some weird shit here
   # overlap from beginning of s1 and end of s2
@@ -42,51 +40,29 @@ def find_shortest_superstring(s1, s2):
   return smaller + bigger
 
 
-def add_to_queue(i, j):
-  s1 = reads[i]
-  s2 = reads[j]
-  combined = find_shortest_superstring(s1, s2)
-  #print("combined " + s1 + " and " +s2+ " to find " + combined)
-  overlap = len(s1) + len(s2) - len(combined)
-  queue.put((-overlap, combined, i, j))
-
-
 for line in fileinput.input():
     line = line.strip()
     reads.append(line)
 
-# loop over n choose 2 pairs of short reads
-# and add them to the queue
-for i in range(len(reads)-1):
-  s1 = reads[i]
-  for j in range(i+1, len(reads)):
-    add_to_queue(i, j)
-    
-# Get the most overlapping pair and process it,
-# cancel out the old strings, add the new one in
-while queue.qsize() > 0:
-  # pop from the queue to see which reads should be merged next
-  best_pair_info = queue.get()
-  i, j = best_pair_info[2:]
-  combined_string = best_pair_info[1]
-  
-  # if either string has previous been merged, continue...
-  if reads[i] == None or reads[j] == None:
-    continue
+best_superstring = None
+best_index = None
+best_overlap = None
 
-  # cancel out the now-merged reads
-  reads[i] = None
-  reads[j] = None
+while len(reads) != 1:
+  r1 = reads.pop(0)
+  for i in range(len(reads)):
+    r2 = reads[i]
+    concat = find_shortest_superstring(r1, r2)
+    overlap = len(r1) + len(r2) - len(concat)
+    if best_overlap == None or overlap > best_overlap:
+      best_overlap = overlap
+      best_index = i
+      best_concat = concat
+  reads.pop(best_index)
+  reads.append(best_concat)
+  best_superstring = None
+  best_index = None
+  best_overlap = None
 
-  # append the new, larger read
-  reads.append(combined_string)
-
-  # compare remaining short reads to this new combined read
-  # and add the comparisons to the queue
-  for i in range(len(reads)-1):
-    if reads[i] != None:
-      add_to_queue(i, len(reads)-1)
-
-print(reads[-1])
-   
+print(reads[0])
 
